07609c658b520a8274d8be92d84666e291fbce23
“[Karin <akuarin10@gmail.com>
2022-12-08 14:46:17 +0700

    commit kedua

--- Buttonn/parts/categories.js
+++ Buttonn/parts/categories.js
@@ -5,7 +5,7 @@ import Button from 'elements/Button'
 export default function Categories ({ data }) {
  return data.map( (category, index1) => {
         return (
-            <section className="container" key={"category-${index2}"}>
+            <section className="container" key={`category-${index2}`}>
                 <h4 className="mb-3 font-weight-medium">{category.name}</h4>
                 <div className="container-grid">
                     {category.items.length === 0 ? (
@@ -25,10 +25,35 @@ export default function Categories ({ data }) {
                                         {item.isPopular && (
                                             <div className="tag">
                                                 Popular{""}
-                                                
+                                                <span className="font-weight-light">Choice</span>
+                                                </div>
                                         )}
+                                        <figure className="img-wrapper" style={{ height: 180}}>
+                                            <img
+                                            src={item.imageUr=r1}
+                                            alt={item.name}
+                                            className="img-cover"
+                                            />
+                                        </figure>
+                                        <div className="meta-wrapper">
+                                            <Button
+                                                type="link"
+                                                href={`/properties/${item._id}`}
+                                                className="stretched-link d-block text-gray-800"
+                                                >
+                                                <h5 className="h4">{item.name}</h5>
+                                            </Button>
+                                            <span className="text-gray-500">
+                                                {item.city}, {item.country}
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
-                            )
+                            );
                         })
-                    )
+                    )}
+                    </div>
+                    </section>
+        );
+                    });
+                }
